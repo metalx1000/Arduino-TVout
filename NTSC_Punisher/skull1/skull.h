70d7635d54805d394fe8c8d8434126ccd087fd66
metalx1000 <git@filmsbykris.com>
2016-06-22 20:05:29 -0400

    Added Punsiher Example code

---
+++ NTSC_Punisher/skull1/skull.h
@@ -0,0 +1,6 @@
+#include <avr/pgmspace.h>
+#ifndef SKULL_H
+#define SKULL_H
+
+extern const unsigned char skull[];
+#endif
---
+++ NTSC_Punisher/skull1/skull2.h
@@ -0,0 +1,6 @@
+#include <avr/pgmspace.h>
+#ifndef SKULL2_H
+#define SKULL2_H
+
+extern const unsigned char skull2[];
+#endif
